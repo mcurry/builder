b716cbb50df32ad2dbc4ad8e9fd73cc61c16fdb8
mcurry <matt@pseudocoder.com>
2009-09-11 09:31:41 -0400

    fixed indents

--- models/build.php
+++ models/build.php
@@ -4,17 +4,17 @@ class Build extends AppModel {
 	var $name = 'Build';
 	var $belongsTo = array('Instance');
 	var $hasMany = array('Test' => array('dependent' => true));
-  var $order = 'start DESC';
-	
+	var $order = 'start DESC';
+
 	var $startTime = null;
-	
+
 	static $pending = 1;
 	static $running = 2;
 	static $completed = 3;
-		
+
 	function start($instance_id, $source, $version=null, $branch=null) {
 		$this->startTime = time();
-		
+
 		$this->create(array('instance_id' => $instance_id,
 												'start' => date('Y-m-d H:i:s', $this->startTime),
 												'state' => self::$running,
@@ -25,18 +25,18 @@ class Build extends AppModel {
 											 ));
 		$this->save();
 	}
-	
+
 	function setStatus($status) {
 		$this->saveField('status', $status);
 	}
 
 	function end($success, $extra=array()) {
-		if(empty($this->id)) {
+		if (empty($this->id)) {
 			return;
 		}
-		
+
 		$extra = array_merge(array('status' => null, 'tests_passed' => 0, 'tests_run' => 0), $extra);
-		
+
 		$end = time();
 		$this->save(array('end' => date('Y-m-d H:i:s', $end),
 											'build_time' => $end - $this->startTime,
